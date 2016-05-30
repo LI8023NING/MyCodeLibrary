c2cc34244abec27a98d8b96e8729f7c44fb2b18f
SeeYouTomorrow <li_ning_@outlook.com>
2016-05-30 13:14:45 +0800

    搜索二叉树转双向链表 加测试

--- BinaryTreeDoubleList.h
+++ BinaryTreeDoubleList.h
@@ -181,3 +181,21 @@ public:
 public:
 	SBTNode<K, V> *_root;
 };
+
+
+
+void Test()
+{
+	int a[] = { 5, 3, 4, 1, 7, 2, 6 };
+	SBTree<int, int> s1;
+	for (int i = 0; i < sizeof(a) / sizeof(int); ++i)
+	{
+		s1.Insert(a[i], a[i]);
+	}
+
+	cout << "二叉树的中序遍历结果：" << endl;
+	s1.InOrder(s1._root);
+	cout << endl;
+	cout << "双向链表的遍历结果：" << endl;
+	s1.TreaveList();
+}
