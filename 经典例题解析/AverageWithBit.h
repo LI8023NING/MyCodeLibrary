d5deef03d9bcc4c7cab361e3db8f0b09289e690b
SeeYouTomorrow <li_ning_@outlook.com>
2016-07-06 21:42:19 +0800

    添加解释说明关于位运算求两个数的平均值

--- 经典例题解析/AverageWithBit.h
+++ 经典例题解析/AverageWithBit.h
@@ -7,10 +7,10 @@ int AverageForTwoNumber(int a,int b)
 }
 
 //解释：a与b的结果是：a和b的二进制相同位，有相同的值，与的结果就是本身
-//       也可以是两个相同值的平均值，
+//   也可以是两个相同值的平均值，a^b的结果：即不同为1，相当相加，右移1，相当除二；
 
 void Test()
 {
 	cout << AverageForTwoNumber(2,4)<< endl;  //3
-	cout << AverageForTwoNumber(3,5)<< endl;  //4
+	cout << AverageForTwoNumber(2,5)<< endl;  //4
 }
