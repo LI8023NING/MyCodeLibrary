e6acef7a31908aa67b3372d27b1740e84db4cc81
SeeYouTomorrow <li_ning_@outlook.com>
2016-07-15 22:19:44 +0800

    添加日历查询功能

--- C实现万年历/CalSelect.cpp
+++ C实现万年历/CalSelect.cpp
@@ -2,6 +2,7 @@
 #include"HeadFile.h"
 #include"AllShengMing.h"
 
+//查询列表
 void CalSelect(CDateTime *dt)
 {
 	int select = 1;
@@ -11,7 +12,7 @@ void CalSelect(CDateTime *dt)
 		printf("*****************************************\n");
 		printf("*[1] 查询当前日期      [2] 查询当前时间 *\n");
 		printf("*[3] 查询日期时间      [4] 查询星期     *\n");
-		printf("*[5] 查询N天后日期     [6] 查询N天前日期*\n");
+		printf("*[5] 查询N天前日期     [6] 查询N天后日期*\n");
 		printf("*[7] 查询距离某日的天数[0] 返回主菜单   *\n");
 		printf("*****************************************\n");
 		printf("请选择:>");
@@ -46,7 +47,7 @@ void CalSelect(CDateTime *dt)
 	}
 }
 
-
+//查询现在日期
 void SelectNowDate(CDateTime *dt)
 {
 	GetNowDateTime(dt);
@@ -54,12 +55,15 @@ void SelectNowDate(CDateTime *dt)
 	system("pause");
 }
 
+//查询现在时间
 void SelectNowTime(CDateTime *dt)
 {
 	GetNowDateTime(dt);
 	printf("%d时%d分%d秒\n", dt->m_hour, dt->m_minute, dt->m_second);
 	system("pause");
 }
+
+//查询时间和日期
 void SelectDateTime(CDateTime *dt)
 {
 	GetNowDateTime(dt);
@@ -67,6 +71,8 @@ void SelectDateTime(CDateTime *dt)
 	printf("%d时%d分%d秒\n", dt->m_hour, dt->m_minute, dt->m_second);
 	system("pause");
 }
+
+//查询周几
 void SelectWeek(CDateTime *dt)
 {
 	int year = 0;
@@ -94,17 +100,105 @@ void SelectWeek(CDateTime *dt)
 //N天前日期
 void SelectLastNday(CDateTime *dt)
 {
+	int N;
+	printf("需要知道几天前日期:)\n");
+	scanf("%d",&N);
 
+	GetNowDateTime(dt);
+
+	dt->m_day -= N;
+	while (dt->m_day <= 0)
+	{
+		if (dt->m_month == 1)
+		{
+			dt->m_year--;
+			dt->m_month = 12;
+		}
+
+		else
+		{
+			dt->m_month--;
+		}
+
+		dt->m_day += GetDays(dt->m_year, dt->m_month);
+	}
+	printf("%d年%d月%d日\n", dt->m_year, dt->m_month, dt->m_day);
+	system("pause");
 }
 
 //N天后日期
 void SelectNextNday(CDateTime *dt)
 {
+	int N;
+	printf("需要知道几天后日期:)\n");
+	scanf("%d", &N);
 
+	GetNowDateTime(dt);
+
+	dt->m_day += N;
+	while (dt->m_day > GetDays(dt->m_year, dt->m_month))
+	{
+		dt->m_day -= GetDays(dt->m_year, dt->m_month);
+		if (dt->m_month == 12)
+		{
+			dt->m_year++;
+			dt->m_month = 1;
+		}
+		else
+		{
+			dt->m_month++;
+		}
+	}
+	printf("%d年%d月%d日\n", dt->m_year, dt->m_month, dt->m_day);
+	system("pause");
 }
 
 //距离某日的天数
 void SelectFutureDay(CDateTime *dt)
 {
+	int flag = 1;
+	int y, m, d;
+	printf("输入需要计算的日期:)\n");
+	scanf("%d%d%d",&y,&m,&d);
+	CDateTime *max = (CDateTime*)malloc(sizeof(CDateTime));
+	max->m_day = d;
+	max->m_month = m;
+	max->m_year = y;
+	CDateTime *min = (CDateTime*)malloc(sizeof(CDateTime));
+	GetNowDateTime(min);
+	
+	if (max->m_year < min->m_year
+		|| (max->m_year == min->m_year && max->m_month < min->m_month)
+		|| (max->m_year == min->m_year && max->m_month == min->m_month) && max->m_day < min->m_day)
+	{
+		CDateTime *tmp = nullptr;
+		tmp = min;
+		min = max;
+		max = tmp;
 
+		flag = -1;
+	}
+
+	int days = 0;
+	while (!(max->m_year == min->m_year && max->m_month == min->m_month && max->m_day == min->m_day))
+	{
+		min->m_day += 1;
+		while (min->m_day > GetDays(min->m_year, min->m_month))
+		{
+			min->m_day -= GetDays(min->m_year, min->m_month);
+			if (min->m_month == 12)
+			{
+				min->m_year++;
+				min->m_month = 1;
+			}
+			else
+			{
+				min->m_month++;
+			}
+		}
+		days++;
+	}
+		
+	printf("两个日期相隔天数是: %d\n",flag*days);
+	system("pause");
 }
