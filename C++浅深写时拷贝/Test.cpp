cb0eb42271ca4c0cc9bd0985676f5d672612d02c
SeeYouTomorrow <li_ning_@outlook.com>
2016-07-23 11:21:15 +0800

    添加深拷贝，即string的正确写法

--- C++浅深写时拷贝/Test.cpp
+++ C++浅深写时拷贝/Test.cpp
@@ -57,3 +57,84 @@ namespace A
 		return 0;
 	}
 }
+
+//深拷贝
+namespace B
+{
+	/////////////////////////////////////////
+	//面试中的string这样写
+	//////////////////////////////////////////
+#include<iostream>
+#include<string.h>
+	using namespace std;
+
+	class String
+	{
+	public:
+		String()
+			:_data(new char[1])
+		{
+			*_data = '\0';
+		}
+
+		String(const char* str)
+			:_data(new char[strlen(str) + 1])
+		{
+			strcpy(_data, str);
+		}
+
+		String(const String& str)
+			:_data(new char[str.size() + 1])
+		{
+			strcpy(_data, str.c_str());
+		}
+
+		~String()
+		{
+			delete[] _data;
+		}
+
+		// Traditional:
+		/*String& operator=(const String& str)
+		{
+		string tmp(str._data);
+		swap(tmp._data);
+		return *this;
+		}*/
+		String& operator=(const &String str) //value
+		{
+			swap(str);
+			return *this;
+		}
+
+		size_t size() const
+		{
+			return strlen(_data);
+		}
+
+		const char* c_str() const
+		{
+			return _data;
+		}
+
+		void swap(String& str)
+		{
+			std::swap(_data, str._data);
+		}
+	private:
+		char* _data;
+	};
+
+	void Test()
+	{
+		string str;
+		str.append(1, 'f');
+		cout << str << endl;
+	}
+
+	int main()
+	{
+		Test();
+		return 0;
+	}
+}
