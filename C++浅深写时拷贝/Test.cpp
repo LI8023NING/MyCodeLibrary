1afc764ad1cf1c4bd84be604656286d499b473e6
SeeYouTomorrow <li_ning_@outlook.com>
2016-07-23 11:27:37 +0800

    添加写时拷贝：方法：设置引用计数

--- C++浅深写时拷贝/Test.cpp
+++ C++浅深写时拷贝/Test.cpp
@@ -138,3 +138,199 @@ namespace B
 		return 0;
 	}
 }
+
+
+namespace C
+{
+	//class String
+	//{
+	//public:
+	//	String(const char* str)
+	//		:_str(new char[strlen(str) + 1])
+	//		,_pCount(new int(1))
+	//	{
+	//		strcpy(_str, str);
+	//	}
+
+	//	String(String& s)
+	//		:_str(s._str)
+	//		,_pCount(s._pCount)
+	//	{
+	//		++(*_pCount);
+	//	}
+
+	//	String& operator=(const String& s)
+	//	{
+	//		if (_str != s._str)
+	//		{
+	//			// 释放原对象
+	//			if (--(*_pCount) == 0)
+	//			{
+	//				delete _pCount;
+	//				delete[] _str;
+	//			}
+
+	//			// 浅拷贝并增加引用计数
+	//			_str = s._str;
+	//			_pCount = s._pCount;
+	//			++(*_pCount);
+	//		}
+
+	//		return *this;
+	//	}
+
+	//	~String()
+	//	{
+	//		if (--(*_pCount) == 0)
+	//		{
+	//			cout<<"delete[] _str"<<endl;
+	//			delete _pCount;
+	//			delete[] _str;
+	//		}
+	//	}
+
+	//private:
+	//	char* _str;
+	//	//static int _count;
+
+	//	int* _pCount;
+	//};
+
+	class String
+	{
+	public:
+		String(const char* str)
+			:_str(new char[strlen(str) + 5])
+		{
+			_str += 4;
+			strcpy(_str, str);
+
+			_GetRefCount(_str) = 1;
+		}
+
+		String(const String& s)
+			:_str(s._str)
+		{
+			++_GetRefCount(_str);
+		}
+
+		String& operator=(const String& s)
+		{
+			if (_str != s._str)
+			{
+				_Release();
+
+				_str = s._str;
+				++_GetRefCount(_str);
+			}
+
+			return *this;
+		}
+
+		~String()
+		{
+			_Release();
+		}
+
+		// Insert
+		// Find
+
+		char& operator[](size_t pos)//String* this
+		{
+			// 当引用计数大于1，需要完成写时拷贝
+			if (_GetRefCount(_str) > 1)
+			{
+				char* tmp = new char[strlen(_str) + 5];
+				tmp += 4;
+				strcpy(tmp, _str);
+				_GetRefCount(tmp) = 1;
+
+				--_GetRefCount(_str);
+				_str = tmp;
+			}
+
+			return _str[pos];
+		}
+
+		//const String* this
+		const char& operator[](size_t pos) const
+		{
+			return _str[pos];
+		}
+
+		int Find(const String& s)
+		{
+			cout << "Find(const String& s)" << endl;
+			return -1;
+		}
+
+	private:
+		int& _GetRefCount(char* _ptr)
+		{
+			return *((int*)(_ptr - 4));
+		}
+
+		void _Release()
+		{
+			if (--_GetRefCount(_str) == 0)
+			{
+				delete[](_str - 4);
+			}
+		}
+
+	private:
+		char* _str;
+	};
+
+	//int String::_count = 0;
+
+	// Copy On Write
+
+	void Test1()
+	{
+		String s1("1111111111111111111111");
+		String s2(s1);
+
+		//// 读时也拷贝
+		//cout<<s1[0]<<endl;
+
+		//// 写时拷贝
+		//s1[0] = 'x';
+
+		s1.Find("xxxx");
+		s1.Find(s2);
+	}
+}
+
+void Test5()
+{
+	int begin = GetTickCount();
+
+	COW::String s1("1111111111111111111111111111111111111111");
+	for (int i = 0; i < 1000000; ++i)
+	{
+		COW::String s2(s1);
+		// cout<<s2<<endl;
+		//...
+	}
+
+	int end = GetTickCount();
+
+	cout << end - begin << endl;
+}
+
+#include <string>
+
+int main()
+{
+
+	COW::Test1();
+	//Test5();
+
+	//string s1("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	//string s2(s1);
+
+	//printf("%x, %x", s1.c_str(), s2.c_str());
+
+	return 0;
+}
